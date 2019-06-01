641b2dde45c878ef2d810c09c54d1e8dbae0db90
Matt Arrick <mattarrick@gmail.com>
2019-06-01 20:42:35 +0000

    Carrierwave2

--- Gemfile
+++ Gemfile
@@ -75,3 +75,7 @@ gem 'simple_form'
 gem 'devise'
 
 gem 'carrierwave', '~> 1.0'
+
+gem "figaro"
+
+gem "fog-aws"
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,10 +6,4 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
   end
-
-  private
-
-  def course_params
-    params.require(:course).permit(:image)
-  end
 end
