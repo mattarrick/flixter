2c30843e8a8549182963ecfce5cf4236c36d7da5
Matt Arrick <mattarrick@gmail.com>
2019-06-04 16:53:01 +0000

    Reinstalling Figaro and finish Carrierwae

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  # config.fog_provider = 'fog/aws'                        # required
-  config.storage    = :aws
+  config.fog_provider = 'fog/aws'                        # required
+  # config.storage    = :aws
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
