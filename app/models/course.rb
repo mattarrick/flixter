93c01e7f2c00067e21d4ab9de8cfd2384a495b18
Matt Arrick <mattarrick@gmail.com>
2019-06-05 04:38:15 +0000

    Student Enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,11 @@
 class Course < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   has_many :sections
+  has_many :enrollments
 
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
-
-  mount_uploader :image, ImageUploader
 end
