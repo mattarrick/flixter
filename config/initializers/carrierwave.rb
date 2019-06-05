6106a53cf9e475792c3b1b6b4bc4f5e573febe2a
Matt Arrick <mattarrick@gmail.com>
2019-06-05 01:27:55 +0000

    Video Embedding

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,6 +11,11 @@ CarrierWave.configure do |config|
     secret_access_key: ENV["AWS_SECRET_KEY"],
     region:            ENV["AWS_REGION"]
   }
+
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
   # config.fog_credentials  = {
    # provider:              'AWS',                        # required
    #  aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
