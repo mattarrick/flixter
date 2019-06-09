d82c74f93258ae09888c9f48c56ae9af953912b7
Matt Arrick <mattarrick@gmail.com>
2019-06-09 20:01:30 +0000

    New section form modal2

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:create]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
